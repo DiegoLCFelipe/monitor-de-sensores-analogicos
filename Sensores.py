002a85eeaff852af47397819ce2305ca1a0c238a
Diego Felipe <diego.felipe095@gmail.com>
2022-06-30 18:20:48 -0300

    feat:criação de classe para animação

--- Sensores.py
+++ Sensores.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import numpy as np
 
 
-class Sensores():
+class Sensores:
 
     def __init__(self, porta_serial='/dev/ttyACM0', pino='a:0:i', resistencia=10000):
         self.placa = Arduino(porta_serial)
@@ -18,9 +18,6 @@ class Sensores():
     def sinal_padrao_arduino(self):
         return 1023 * self.registro.read()
 
-    def serie_temporal(self, formato='%H:%M:%S'):
-        return datetime.now().strftime(formato)
-
 
 class NTC(Sensores):
     COEF_STEINHART_1 = 1.009249522e-03
@@ -28,8 +25,9 @@ class NTC(Sensores):
     COEF_STEINHART_3 = 2.019202697e-07
 
     def __next__(self):
-        if self.registro.read() != None:
-            return self.serie_temporal(), self.calcula_temperatura()
+        if self.registro.read() is not None:
+            return self.calcula_temperatura()
+
 
     def calcula_resistencia_thermistor(self):
         return self.resistencia * (1023 / self.sinal_padrao_arduino() - 1)
@@ -40,8 +38,8 @@ class NTC(Sensores):
 
     def calcula_temperatura(self, unidade="Celcius"):
         temperatura = 1.0 / (
-                    self.COEF_STEINHART_1 + self.COEF_STEINHART_2 * np.log(self.calcula_resistencia_thermistor())
-                    + self.COEF_STEINHART_3 * np.log(self.calcula_resistencia_thermistor()) ** 3)
+                self.COEF_STEINHART_1 + self.COEF_STEINHART_2 * np.log(self.calcula_resistencia_thermistor())
+                + self.COEF_STEINHART_3 * np.log(self.calcula_resistencia_thermistor()) ** 3)
         if unidade == 'Kelvin':
             return temperatura
         elif unidade == 'Celcius':
--- main.py
+++ main.py
@@ -1,41 +1,109 @@
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib.widgets import Slider, Button
+from matplotlib.lines import Line2D
+
 from Sensores import NTC
 
-x = []
-y = []
+
+class Scope:
+    def __init__(self, variavel):
+        self.amplitude_slider = None
+        self.y0_slider = None
+        self.periodo_slider = None
+        self.amp_slider = None
+        self.variavel = variavel
+        plt.style.context('dark_background')
+        self.fig, self.ax = plt.subplots(figsize=(12, 8))
+        self.time_data = [0]
+        self.sensor_data = [0]
+        self.dt = 0
+        self.line = Line2D(self.time_data, self.sensor_data)
+        self.ax.add_line(self.line)
+        self.line.set_linewidth(3)
+        self.ax.set_ylim(20, 30)
+        self.ax.set_xlim(0, 10)
+        plt.subplots_adjust(left=0.3, bottom=0.25)
 
 
-def valor_slider(slider, val=3):
-    fig.canvas.draw_idle()
-    return slider.val
+    def update(self, data):
+        self.ax.figure.canvas.draw()
+        self.dt += 1
+        self.time_data.append(self.dt)
+        self.sensor_data.append(data)
 
+        if len(self.time_data) > 100:
+            self.time_data = self.time_data[-600:]
+            self.sensor_data = self.sensor_data[-600:]
 
-def plota_temperatura(i, x, y):
-    leitura = next(termistor)
-    x.append(leitura[0])
-    y.append(leitura[1])
+        self.time_data = self.time_data
+        self.sensor_data = self.sensor_data
 
-    x = x[-valor_slider(slider_de_amostras):]
-    y = y[-valor_slider(slider_de_amostras):]
+        self.ax.set_xlim(self.time_data[-1] - self.periodo_slider.val, self.time_data[-1])
 
-    ax.cla()
-    ax.plot(x, y)
+        self.ax.set_ylim(self.y1_slider.val + self.y2_slider.val - (self.amplitude1_slider.val + self.amplitude2_slider.val),
+                         self.y1_slider.val + self.y2_slider.val + (self.amplitude1_slider.val + self.amplitude2_slider.val))
 
-    ax.set_title('Registro de Temperatura TNC 103')
-    ax.set_ylabel('Temperatura °C')
+        self.line.set_data(self.time_data, self.sensor_data)
+        self.ax.grid(True)
+        self.ax.set_ylabel('°C')
+
+        return self.line,
+
+    def iniciar_visualizacao(self):
+        ax_periodo = plt.axes([0.4, 0.1, 0.45, 0.03])
+        self.periodo_slider = Slider(ax=ax_periodo,label="Período (s)",valmin=1,valmax=600,valinit=1,valstep=1)
+
+        ax_y1 = plt.axes([0.05, 0.25, 0.0225, 0.63])
+        self.y1_slider = Slider(
+            ax=ax_y1,
+            label="Y1",
+            valmin=0,
+            valmax=125,
+            valinit=26,
+            orientation="vertical",
+            valstep=1
+        )
+
+        ax_y2 = plt.axes([0.10, 0.25, 0.0225, 0.63])
+        self.y2_slider = Slider(
+            ax=ax_y2,
+            label="Y2",
+            valmin=0,
+            valmax=0.9,
+            valinit=0,
+            orientation="vertical",
+            valstep=0.1
+        )
+
+        ax_amplitude1 = plt.axes([0.15, 0.25, 0.0225, 0.63])
+        self.amplitude1_slider = Slider(
+            ax=ax_amplitude1,
+            label="A1",
+            valmin=0,
+            valmax=10,
+            valinit=1,
+            orientation="vertical",
+            valstep=1
+        )
+
+        ax_amplitude2 = plt.axes([0.2, 0.25, 0.0225, 0.63])
+        self.amplitude2_slider = Slider(ax=ax_amplitude2,
+            label="A2",
+            valmin=0,
+            valmax=1,
+            valinit=0.1,
+            orientation="vertical",
+            valstep=0.1
+        )
+
+        ani = animation.FuncAnimation(fig=self.fig, func=scope.update,
+                                      frames=self.variavel, interval=1000,
+                                      blit=True)
+        plt.style.context('dark_background')
+        plt.show()
 
 
 termistor = NTC()
-
-fig, ax = plt.subplots(figsize=(12, 8))
-ax_slider_n = plt.axes([0.15, 0.1, 0.75, 0.03])
-
-slider_de_amostras = Slider(ax=ax_slider_n, label='N',
-                            valmin=0, valmax=30, valinit=3, valstep=1)
-ax.xaxis.set_tick_params(rotation=45)
-
-ani = animation.FuncAnimation(fig, plota_temperatura, fargs=(x, y), interval=1000)
-fig.subplots_adjust(bottom=0.25, left=0.15)
-plt.show()
+scope = Scope(termistor)
+scope.iniciar_visualizacao()
