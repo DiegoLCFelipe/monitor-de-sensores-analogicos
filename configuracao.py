798ead2f2bd447c99a24348c47240c5e5700bd3b
Diego Felipe <diego.felipe095@gmail.com>
2022-07-07 19:09:34 -0300

    Criação do arquivo de configuração dos controles

---
+++ configuracao.py
@@ -0,0 +1,27 @@
+
+CONROLES ={
+    # Controle deslizante período
+    'Período mínimo': 2,
+    'Período máximo': 100,
+    'Escala período': 10, 
+
+    # Controle deslizante Y1
+    'Y1 mínimo': 0,
+    'Y1 máximo': 50,
+    'Escala Y1': 1,
+
+    # Controle deslizanete Y2
+    'Y2 mínimo': 0,
+    'Y2 máximo': 0.9,
+    'Escala Y2': 0.1,
+
+    # Controle deslizante amplitude 1
+    'A1 mínima': 0,
+    'A1 máxima': 20,
+    'Escala A1': 1,
+
+     # Controle deslizante amplitude 2
+    'A2 mínima': 0, 
+    'A2 máxima': 1,
+    'Escala A2': 0.1,
+    }
--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from tratamentodesinal import SinalAnalogico
 from estrategiadeconversao.thermistorntc103 import Steinhart
 from grafico import Animacao
+import configuracao
 
 termistor = SinalAnalogico(Steinhart(resistencia=10000, unidade='Celcius'))
-
-configuracao_controles = {'Período mínimo': 2,
-                          'Período máximo': 100,
-                          'Y1 mínimo': 0,
-                          'Y1 máximo': 50,
-                          'A1 máxima': 20}
-
-scope = Animacao(termistor, 'Temperatura (°C)', **configuracao_controles)
+scope = Animacao(termistor, 'Temperatura (°C)', **configuracao.CONROLES)
 scope.iniciar_visualizacao()
